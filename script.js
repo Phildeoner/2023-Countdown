db5b53eab3ccbd2f29b83b7b5fc8e4cef99c3840
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-05 02:02:54 +0100

    Create UpdateTime function to update time

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ let getSecs = document.getElementById("secs");
 
 // Set Count Down date
 let countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+// Update count down timer every 1 second
+let timeUpdate = updateTime(function() {
+
+    // Get the current date and time
+    let currentTimeAndDate = new Date().getTime();
+  
+    // Find the interval between currentTime and the countDownDate
+    let interval = countDownDate - currentTimeAndDate;
+  
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(interval / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((interval % (1000 * 60)) / 1000);
+  
+    // Display the time.
+    getDays.innerHTML = days;
+    getHours.innerHTML = hours;
+    getMins.innerHTML = minutes;
+    getSecs.innerHTML = seconds;
+    
+  }, 1000);
