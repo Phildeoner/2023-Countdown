a74ce3ad0601d36c3497bd49d9663ee8dc6cfe09
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-05 01:59:02 +0100

    Set count down date

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let getDays = document.getElementById("days");
 let getHours = document.getElementById("hours");
 let getMins = document.getElementById("mins");
 let getSecs = document.getElementById("secs");
+
+// Set Count Down date
+let countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
